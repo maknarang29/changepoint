14fa196be53d631582f270ba7e93b2d687aa7cc4
maknarang29 <maknarang29@gmail.com>
2022-03-08 13:36:27 +0000

    data analysis

---
+++ data_analysis.R
@@ -0,0 +1,3 @@
+library(rjson);
+dat <- fromJSON(data = "/Users/manaknarang/MANAK/'Year 4'/changepoint/datasets/quality_control_1/quality_control_1.json")
+data
--- simulation.R
+++ simulation.R
@@ -40,3 +40,23 @@ x1 <- rpois(100,1.5*(1:100)**2)+rnorm(100)**ceiling(runif(1,1,15))
 x2 <- rpois(100,1.5*(100:1)**2)+rnorm(100)**ceiling(runif(1,1,15))
 x <- c(x1,x2)
 plot(x)
+
+
+
+d <- 1
+m_est <- rep(NA,length = 100)
+tau_est <- list()
+for(i in 1:length(m_est)){
+  set.seed(i)
+  x <- c(rnorm(100,mean=-d,sd=1),rnorm(100,mean=d,sd=1))
+  fit <- cpt.mean(x,method = "PELT")
+  m_est[i] <- ncpts(fit)
+  tau_est[[i]] <- cpts(fit)
+}
+table(m_est)
+table(unlist(tau_est[m_est == 1]))
+barplot(table(c(unlist(tau_est[m_est == 1]),1:length(x)))-1)
+
+
+
+unlist(m_est)
